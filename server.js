928def78a43279ecb57b30ee68edb310edab8f12
Salamanilyas <salamanilyas101@gmail.com>
2023-02-22 22:26:45 +0500

    add-commit

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const cors=require("cors")
 const  bodyParser=require("body-parser")
 const dotenv=require("dotenv")
 const db =require( "./database/db")
+const routes =require( "./Routes/AdminRoutes")
 dotenv.config({path:"config/config.env"})
 db()
 const port=process.env.port
 
 const app=express()
+// middlewears 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.urlencoded({extended:false}))
-
+// routes 
+app.use("/api",routes)
+// server 
 app.listen(port,()=>{
     console.log(`server is working on http://localhost:${port}`);
 })
