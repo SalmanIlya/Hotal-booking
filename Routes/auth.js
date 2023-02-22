2f8c577bd8b51fca4664faef523b351f925e4396
Salamanilyas <salamanilyas101@gmail.com>
2023-02-22 22:26:01 +0500

    create-user

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,9 +1,16 @@
 const express=require("express")
+const User=require("../Module/User")
+
 const app=express.Router()
 
-
-
-
+app.post("/create",async(req,res)=>{
+try{
+const createUser=await User.create(req.body)
+res.send(createUser)
+}catch(err){
+    res.send("error")
+}
+})
 
 
 
