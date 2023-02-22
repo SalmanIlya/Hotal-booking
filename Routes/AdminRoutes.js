9570bd9adec05dade71038d031f63e6886047670
Salamanilyas <salamanilyas101@gmail.com>
2023-02-22 22:25:45 +0500

    change name

--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -1,11 +1,13 @@
 const express=require("express")
-const auth=require("./auth")
+// const auth=require("")
 const user=require("./User")
+const auth=require("./auth")
 const Hotal=require("./hotal")
 const app=express.Router()
 
-app.use("/auth",auth)
+// app.use("/auth",auth)
 app.use("/user",user)
+app.use("/user",auth)
 app.use("/hotal",Hotal)
 
 
